74b68a8a8001cb74c9746bd22c6ae33ff0d4d5f5
Ciro-77 <65682718+Ciro-77@users.noreply.github.com>
2021-08-27 20:52:45 -0300

    Create TA TE TI.py
    probandd

---
+++ TA TE TI.py
@@ -0,0 +1,17 @@
+print('TA TE TI')
+print('Proyecto: Cintia, Luciano, Ciro Farias')
+def vertablero(tablero):
+    for fila in tablero:
+        for i in range(len(fila)):
+            if i == len(fila) - 1:
+                print(fila[i], end='\n')
+
+
+
+tablero =   {
+    [' ', '|', ' ', '|', ' ']
+    ['-', '+', '-', '+', '-']
+    [' ', '|', ' ', '|', ' ']
+    ['-', '+', '-', '+', '-']
+    [' ', '|', ' ', '|', ' ']
+      }
